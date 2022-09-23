c4d5c75a5c4e7e67e2a9faa38f394738af6775a7
Reece <99863802+reece-martin@users.noreply.github.com>
2022-09-23 12:33:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,21 +18,28 @@ window.addEventListener('load', () => {
     document.getElementById("finishday").style.display = "none";
      
     function submitText(){
-        // Allows the user to input any number of tasks, with a checkbox system.
-        var createBox = document.createElement("input");
-        var createTask = document.createElement("label");
-        var createBr = document.createElement("br")
-        createBox.value = (document.getElementById("userInput").value);
-        createBox.type = "checkbox";
-        createTask.innerHTML = (document.getElementById("userInput").value);
-        document.getElementById("tasklist").appendChild(createBox);
-        document.getElementById("tasklist").appendChild(createTask);
-        document.getElementById("tasklist").appendChild(createBr);
+        /* Allows the user to input any number of tasks, only tasks with text 
+        will be added to the list. */
+        if ((document.getElementById("userInput").value) !== ""){
 
-        document.getElementById("userInput").value = "";
+            var createBox = document.createElement("input");
+            var createTask = document.createElement("label");
+            var createBr = document.createElement("br")
+            
+            createBox.value = (document.getElementById("userInput").value);
+            createBox.type = "checkbox";
+            createTask.innerHTML = (document.getElementById("userInput").value);
+            document.getElementById("tasklist").appendChild(createBox);
+            document.getElementById("tasklist").appendChild(createTask);
+            document.getElementById("tasklist").appendChild(createBr);
+
+            document.getElementById("userInput").value = "";
+            }
         }
 
     function hideSubmit(){
+        /* Once the user has finished inputting their tasks, displays the tasks 
+        and hides the input components. */
         document.getElementById("submittasks").style.display = "none";
         document.getElementById("userInput").style.display = "none";
         document.getElementById("finishinput").style.display = "none";
@@ -40,12 +47,17 @@ window.addEventListener('load', () => {
         document.getElementById("tasklist").style.display = "block";
     }
 
+
+
     // Calls submitText when button is clicked.
     document.getElementById("submittasks").onclick = submitText;
     // Calls hideSubmit when button is clicked.
     document.getElementById("finishinput").onclick = hideSubmit;
-       
+    
     
 });
 
-// I need an "Add additional tasks" button.
+// I need to center the finishday button which is ignoring the global center.
+// I need to make pressing enter in userInput, activate the submit function.
+// I need an "Add additional/edit tasks" button.
+// Finish my day sould bring up an alert/new screen.
