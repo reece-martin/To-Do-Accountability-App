aef11a6947221d9e296ced19b0b4d985636edb79
Reece <99863802+reece-martin@users.noreply.github.com>
2022-10-12 09:59:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ window.addEventListener('load', () => {
     const day = new Date();
     let today = day.getDay();
     document.getElementById("today").innerHTML = (daysofweek[today]);
-
+    
     // Hides/shows the necessary buttons to start the app.
     document.getElementById("finishday").style.display = "none";
     document.getElementById("addedit").style.display = "none";
@@ -33,14 +33,18 @@ window.addEventListener('load', () => {
             
             createBox.value = (document.getElementById("userInput").value);
             createBox.type = "checkbox";
+            
             // this is what i'm trying to reference for greying out complete tasks.
             createBox.name = "checkboxes";
             //testing area
             createBox.className = "listbox";
             //testing area
+            
             createTask.innerHTML = (document.getElementById("userInput").value);
             document.getElementById("tasklist").appendChild(createBox);
             document.getElementById("tasklist").appendChild(createTask);
+            //Allows the task to be edited by clicking on it's text. Huge win for me.
+            createTask.setAttribute("contentEditable", "true")
             document.getElementById("tasklist").appendChild(createBr);
 
             document.getElementById("userInput").value = "";
@@ -139,6 +143,5 @@ window.addEventListener('load', () => {
 // Can't click "thats all my tasks" without submitting at least 1 task.
 // If you click finsh day without completing all tasks. A warning apears.
 // import random inspirational quote from API.
-// onclick turn existing task into submission box with previous text as placeholder.
 // Grey out completed tasks.
 // Visual improvements on background, buttons, and animations.
